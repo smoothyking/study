9862ba30116f1b159768e7829d7bf123d19cb7b6
최승훈 <gogo5896544@naver.com>
2024-09-05 10:25:13 +0900

    complete file

--- src/main/java/org/example/Study5.java
+++ src/main/java/org/example/Study5.java
@@ -1,4 +1,12 @@
 package org.example;
 
 public class Study5 {
+    public static void main(String[] args) {
+        int num = 0;
+        while(num <= 3) {
+            //num;
+            System.out.println(num++);
+        }
+    }
 }
+
